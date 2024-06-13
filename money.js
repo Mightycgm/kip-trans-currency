6b4394eee7356812cf31a2f6fdcaa1c42f499bd8
Mightycgm <118869637+Mightycgm@users.noreply.github.com>
2024-06-13 09:21:47 +0700

    Add files via upload

---
+++ money.js
@@ -0,0 +1,6 @@
+function del() {
+    let number1 = document.getElementById("number1");
+    let result = document.getElementById("result");
+    let sum = parseInt(number1.value) *590.81 ;
+    result.innerHTML = sum;
+}
